47ee9de4d32b8bed18f2a4b991d1632781deac3c
Auroratide <tf.auroratide@gmail.com>
2016-07-27 15:20:21 -0500

    First Account Test fails

---
+++ src/main/java/com/thoughtworks/tddintro/accountbalance/Account.java
@@ -0,0 +1,37 @@
+package com.thoughtworks.tddintro.accountbalance;
+
+/**
+ * Created by Timothy Foster on 27 Jul 2016.
+ *
+ * For now we assume all balances are integer values (in whole dollars).
+ */
+public class Account {
+
+/*  Constructors
+ *  =========================================================================*/
+    public Account() {
+        this(0);
+    }
+
+    public Account(int initialBalance) {
+        balance = initialBalance;
+    }
+ 
+/*  Accessors
+ *  =========================================================================*/ 
+    public int getBalance() {
+        return balance;
+    }
+ 
+/*  Public Methods
+ *  =========================================================================*/
+    public void deposit(int amount) {
+        // do nothing
+    }
+ 
+/*  Private Members
+ *  =========================================================================*/
+    private int balance;
+
+
+}
