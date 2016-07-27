4252640e0833e4da7f370c3bb636655746fc5d98
Auroratide <tf.auroratide@gmail.com>
2016-07-27 15:04:01 -0500

    All tests pass for Factorial

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -2,6 +2,8 @@ package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
+        if(i < 0)
+            throw new IllegalArgumentException("Attempted " + i + "!, but cannot take the factorial of a negative number");
         if(i <= 1)
             return 1;
         else
